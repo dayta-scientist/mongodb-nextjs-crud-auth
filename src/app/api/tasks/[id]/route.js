4c3e353ac3993c5df657a0cfc71820c8b8d2a95c
Gabriel LR <dayta.scientist@gmail.com>
2023-09-06 07:04:03 -0300

    PUT in src/app/api/tasks/[id]/route.js

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,10 +1,25 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/utils/mongoose";
+import Task from "@/models/Task";
 
-export function GET (request, {params}) {
-  console.log(params)
-  return NextResponse.json({
-    message: `getting task ${params.id}`
-  })
+export async function GET (request, {params}) {
+  try {
+    connectDB()
+    const taskFound = await Task.findById(params.id)
+
+    if (!taskFound) {
+      return NextResponse.json({
+        message: "Task not found",
+      }, {
+        status: 404
+      })
+    }
+    return NextResponse.json(taskFound)
+  } catch (error) {
+    return NextResponse.json(error.message, {
+      status: 400,
+    })
+  }
 }
 
 export function DELETE (request, {params}) {
@@ -14,9 +29,17 @@ export function DELETE (request, {params}) {
   })
 }
 
-export function PUT (request, {params}) {
-  console.log(params)
-  return NextResponse.json({
-    message: `updating task ${params.id}`
-  })
+export async function PUT (request, {params}) {
+  try {
+    const data = await request.json()
+    const taskUpdated = await Task.findByIdAndUpdate(params.id, data, {
+      new: true
+    })
+
+    return NextResponse.json(taskUpdated)
+  } catch (error) {
+    return NextResponse.json(error.message, {
+      status: 400
+    })
+  }
 }
