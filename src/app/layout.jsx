f17c7a5b66bfce3df04c41d5bedb854be592902c
Gabriel LR <dayta.scientist@gmail.com>
2023-09-06 20:14:25 -0300

    Navigation in src/components/TaskCard.jsx and not-found.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 export default function RootLayout({ children }) {
   return (
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
+        <Navbar />
         <main className='container mx-auto px-5 mt-4'>
           {children}
         </main>
